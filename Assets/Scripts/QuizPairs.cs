2f8b0984d6a61e81ee922d0e841ee8cd423e3db9
Kishan Takoordyal <kishan@kinesis.games>
2020-03-10 15:26:12 +0400

    Finish core logic

---
+++ Assets/Scripts/QuizPairs.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Quiz Pairs")]
+public class QuizPairs : ScriptableObject {
+    public string question;
+    public string[] answers = new string[4];
+}
